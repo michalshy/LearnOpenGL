00aa87df01f1a33ebe9e5c52f3ed8d4bfbada2ee
michin <niesmialymichal@gmail.com>
2025-02-28 12:59:22 +0100

    Move functions to cpp from hpp for lesson shaders

---
+++ LearnOpenGL/src/lessons/LessonShaders.cpp
@@ -0,0 +1,61 @@
+#include "LessonShaders.hpp"
+
+void LessonShaders::init()
+{
+	float first[] = {
+		//positions		  //colors
+	 0.2f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // right top
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // right bottom right
+	 0.2f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, };  // right bottom left
+	float second[] = {
+	-0.2f,  -0.5f, 0.0f,   // left bottom right
+	-0.5f,  -0.5f, 0.0f,   // left bottom left
+	-0.2f,  0.5f, 0.0f,   // left top
+	};
+	//unsigned int indices[] = {  // note that we start from 0!
+	//	0, 1, 2,   // first triangle
+	//	3, 4, 5    // second triangle
+	//};
+
+	glGenBuffers(1, &VBO1);
+	glGenVertexArrays(1, &VAO1);
+	glBindVertexArray(VAO1);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(first), first, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glGenBuffers(1, &VBO2);
+	glGenVertexArrays(1, &VAO2);
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(second), second, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+}
+
+void LessonShaders::run()
+{
+	sF.use();
+	glBindVertexArray(VAO1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	float timeValue = glfwGetTime();
+	float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+	sS.use();
+	sS.set4Float("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+	glBindVertexArray(VAO2);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(0);
+}
